d34201e2e09fa95d5ff59286c3bb070b1bb2a998
woozyfr <alex@woozy.fr>
2020-10-23 14:31:08 +0200

    change color of modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SideNav from './components/SideNav/SideNav';
 import MapLeaflet from './components/Map/MapLeaflet';
+//import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -8,8 +9,20 @@ function App() {
     <div id="container">
       <SideNav />
       <MapLeaflet map_id="map"/>
+
+     
     </div>
   );
 }
-
+/*
+   <Router>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/reservations' component={Reservations} />
+            <Route path='/choix-pays' component={ChoixPays} />
+            <Route path='/pays/:country_slug' component={Country} />
+          </Switch>
+        </Router>
+*/
 export default App;
